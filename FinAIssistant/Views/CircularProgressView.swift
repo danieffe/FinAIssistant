4e90cf319e61b3dd6b9dc571e99c315792f87fc7
Daniele Fontana <danyfo00@gmail.com>
2024-12-09 12:26:05 +0100

    CircularProgressViewupdates

--- FinAIssistant/Views/CircularProgressView.swift
+++ FinAIssistant/Views/CircularProgressView.swift
@@ -12,19 +12,37 @@ struct CircularProgressView: View {
     
     var body: some View {
         ZStack {
-            // Ogni categoria viene rappresentata come un arco concentricamente più piccolo
+            // Cerchio di base per ogni categoria con un gradiente meno chiaro
             ForEach(0..<categories.count, id: \.self) { index in
+                let category = categories[index]
+                let lighterColor = category.color.opacity(0.5) // Un colore più chiaro ma meno trasparente
+                
+                // Cerchio non colorato con gradiente meno chiaro
                 Circle()
-                    .trim(from: 0, to: categories[index].progress) // Imposta la porzione da colorare
+                    .trim(from: 0, to: 1) // Cerchio intero non colorato
                     .stroke(
-                        categories[index].color,
+                        LinearGradient(
+                            gradient: Gradient(colors: [lighterColor, lighterColor.opacity(0)]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
                         style: StrokeStyle(lineWidth: 20, lineCap: .round)
                     )
                     .rotationEffect(.degrees(-90)) // Inizia il riempimento da ore 12
-                    .frame(width: CGFloat(200 - (index * 30)), height: CGFloat(200 - (index * 30))) // Dimensione del cerchio
+                    .frame(width: CGFloat(200 - (index * 30)), height: CGFloat(200 - (index * 30)))
+                
+                // Cerchio progressivo per ogni categoria
+                Circle()
+                    .trim(from: 0, to: category.progress) // Imposta la porzione da colorare
+                    .stroke(
+                        category.color,
+                        style: StrokeStyle(lineWidth: 20, lineCap: .round)
+                    )
+                    .rotationEffect(.degrees(-90)) // Inizia il riempimento da ore 12
+                    .frame(width: CGFloat(200 - (index * 30)), height: CGFloat(200 - (index * 30)))
             }
         }
-        .frame(width: 200, height: 200) // Dimensione finale del grafico
+        .frame(width: 200, height: 200)
     }
 }
 
